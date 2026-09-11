be08a0ca5f713032ef5856dc51f5900633ec609b
jbrown240 <jbrown240@student.alamo.edu>
2026-09-11 16:07:47 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var myCity = "San Antonio"; // global variable
+console.log(myCity); 
+
+// Block variable 
+{
+    let testScore = 95; 
+    console.log(testScore);
+}
+
+let shoePrice = 100;
+let tax = 10;
+let totalCost = shoePrice + tax; // mathematical operation
+console.log(totalCost);
+
+let studentName = "Jamal Brown"; // string variable
+console.log(studentName);
+
+// Object variable using const 
+const myCar = { make: "Dodge", model: "Charger" }; 
+console.log(myCar);
